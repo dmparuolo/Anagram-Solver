21acbc988367572abcf6f4ce6e9fe8162d2930dd
Dominic Paruolo <142058467+dmparuolo@users.noreply.github.com>
2024-07-18 20:53:22 -0500

    Add files via upload

---
+++ AnagramSolver.java
@@ -0,0 +1,174 @@
+/* CS 314 STUDENTS: FILL IN THIS HEADER.
+ *
+ * Student information for assignment:
+ *
+ *  On my honor, Dominic Paruolo, this programming assignment is my own work
+ *  and I have not provided this code to any other student.
+ *
+ *  UTEID: dmp3588
+ *  email address: dominicparuolo78@gmail.com
+ *  TA name: Nidhi
+ *  Number of slip days I am using: 0
+ */
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class AnagramSolver {
+
+	private HashMap<String, LetterInventory> currentDictionary;
+	
+    /*
+     * pre: list != null
+     * @param list Contains the words to form anagrams from.
+     */
+    public AnagramSolver(Set<String> dictionary) {
+    	if (dictionary == null) {
+    		throw new IllegalArgumentException("Violation of precondtion: AnagramSolver"
+    				+ " Parameter must not be null.");
+    	}
+    	//adds all words from dictionary to a map
+    	currentDictionary = new HashMap<>();
+    	for (String word: dictionary) {
+    		currentDictionary.put(word, new LetterInventory(word));
+    	}
+    }
+
+    /*
+     * pre: maxWords >= 0, s != null, s contains at least one 
+     * English letter.
+     * Return a list of anagrams that can be formed from s with
+     * no more than maxWords, unless maxWords is 0 in which case
+     * there is no limit on the number of words in the anagram
+     */
+    public List<List<String>> getAnagrams(String s, int maxWords) {
+    	if (maxWords < 0 || s == null || !containsEnglishLetter(s)) {
+    		throw new IllegalArgumentException("Violation of precondition: getAnagrams."
+    				+ " Parameter s must not be null and must contain an Englush letter."
+    				+ " Parameter maxWords must be greater than or equal to 0");
+    	}
+    	//the LetterInventory of s
+    	LetterInventory sInventory = new LetterInventory(s);
+    	ArrayList<String> possibleWords = new ArrayList<>();
+    	//finds initial possible words
+    	for (String str: currentDictionary.keySet()) {
+    		if (sInventory.subtract(currentDictionary.get(str)) != null) {
+    			possibleWords.add(str);
+    		}
+    	}
+    	//list of found anagrams
+    	List<List<String>> anagrams = new ArrayList<>();
+    	//current anagram
+    	ArrayList<String> currentAnagram = new ArrayList<>();
+    	findAnagrams(sInventory, possibleWords, anagrams, currentAnagram, maxWords);
+    	for (List<String> gram: anagrams) {
+    		Collections.sort(gram);
+    	}
+    	//sorts the list of anagrams
+    	AnagramComparator c = new AnagramComparator();
+    	Collections.sort(anagrams, c);
+    	return anagrams;
+    }
+    
+    /**
+     * Helper method for getAragrams that finds all the anagrams of a given LetterInventory.
+     */
+    private void findAnagrams(LetterInventory currentLetters, ArrayList<String> possibleWords,
+    		List<List<String>> anagrams, ArrayList<String> currentAnagram, int maxWords) {
+		//base case if no more letters left
+		if (currentLetters.isEmpty()) {
+			//makes copy of current working anagram and adds to list of found anagrams
+			ArrayList<String> foundAnagram = createDeepCopy(currentAnagram);
+			anagrams.add(foundAnagram);
+		//recursive step
+		} else {
+			for (int i = 0; i < possibleWords.size(); i++) {
+				//tests if current letters contain letters of next possible word
+				LetterInventory subtracted = currentLetters.subtract(
+						currentDictionary.get(possibleWords.get(i)));
+				//ensures next word can be part of current available letters
+				//also checks if adding this word will make adding another word impossible
+				//due to max word limit
+				if (subtracted != null && (maxWords == 0 || !(subtracted.size() > 0 
+							&& currentAnagram.size() == maxWords - 1))) {
+					//updates the next anagram with the next word
+					ArrayList<String> newAnagram = createDeepCopy(currentAnagram);
+					newAnagram.add(possibleWords.get(i));
+						//updates possible words
+						ArrayList<String> newWords = createDeepCopy(
+								new ArrayList<>(possibleWords.subList(i, possibleWords.size())));
+						//next recursion call
+						findAnagrams(subtracted, newWords, anagrams, newAnagram, maxWords);
+				}
+			}
+		}
+	}
+    
+    
+    /**
+     * Helper method for getAnagrams that creates a deep copy of a given ArrayList.
+     * @param ogList, the ArrayList to copy.
+     * @return a deep copy of the given ArrayList.
+     */
+    private ArrayList<String> createDeepCopy(ArrayList<String> ogList) {
+    	ArrayList<String> newList = new ArrayList<>();
+    	//add elements of parameter to new list
+    	for (String word: ogList) {
+    		newList.add(word);
+    	}
+    	return newList;
+    }
+    
+    
+    /**
+     * Helper method for getAnagrams that finds if the given String contains an English letter.
+     * @param s != null.
+     * @return true if there is an English letter in the given String or false otherwise.
+     */
+    private boolean containsEnglishLetter(String s) {
+    	if (s == null) {
+    		throw new IllegalArgumentException("Violation of precondtion: containsEnglishLetter"
+    				+ " Parameter must not be null.");
+    	}
+    	//finds if any character is in the English alphabet
+    	for (int i = 0; i < s.length(); i++) {
+    		if ('a' <= s.charAt(i) && s.charAt(i) <= 'z') {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    /**
+     * Helper class for AnagramSolver to sort the ArrayLists of anagrams in order.
+     */
+    private static class AnagramComparator implements Comparator<List<String>> {
+    	
+    	/**
+    	 * Compares two lists returning an integer less than zero if the first list is
+    	 * lexicographically greater than the second list, 0 if the lists are equal, and
+    	 * an integer greater than zero if the first list is lexicographically less than 
+    	 * the second list.
+    	 */
+        public int compare(List<String> a1, List<String> a2) {
+        	//finds if lists are the same size
+        	if (a1.size() != a2.size()) {
+        		return a1.size() - a2.size();
+        	}
+        	//finds which list is smaller or bigger
+        	for (int i = 0; i < a1.size(); i++) {
+        		int result = a1.get(i).compareTo(a2.get(i));
+        		if (result != 0) {
+        			return result;
+        		}
+        	}
+        	//if lists are equal
+        	return 0;
+        }
+    }
+}
---
+++ LetterInventory.java
@@ -0,0 +1,171 @@
+/* CS 314 STUDENTS: FILL IN THIS HEADER.
+ *
+ * Student information for assignment:
+ *
+ *  On my honor, Dominic Paruolo, this programming assignment is my own work
+ *  and I have not provided this code to any other student.
+ *
+ *  UTEID: dmp3588
+ *  email address: dominicparuolo78@gmail.com
+ *  TA name: Nidhi
+ *  Number of slip days I am using: 0
+ */
+
+public class LetterInventory {
+
+	private int size;
+	private final int ALPHABET_SIZE;
+	private int[] frequencies;
+	private final char CHAR_TO_INDEX;
+	
+	/**
+	 * Creates a new LetterInventory object that takes in a String, finds the frequency
+	 * of each letter in the String, storing it in an array, and finds the number of different
+	 * letters in the String.
+	 * @param phrase != null.
+	 */
+	public LetterInventory (String phrase) {
+		if (phrase == null) {
+			throw new IllegalArgumentException("Violation of precondition: LetterInventory."
+					+ " Parameter must not be null.");
+		}
+		size = 0;
+		//size of alphabet
+		ALPHABET_SIZE = 26;
+		frequencies = new int[ALPHABET_SIZE];
+		//the ASCII value distance from the letters to there index in the array
+		CHAR_TO_INDEX = 97;
+		String newPhrase = phrase.toLowerCase();
+		//increments frequency of letters and increments numLetters when new letters are added
+		for (int i = 0; i < newPhrase.length(); i++) {
+			char letter = newPhrase.charAt(i);
+			if ('a' <= letter && letter <= 'z') {
+				//increments frequencies
+				frequencies[letter - CHAR_TO_INDEX] += 1;
+				size++;
+			}
+		}
+	}
+	
+	/**
+	 * Finds the frequency of a letter in this LetterInventory and returns it.
+	 * @param letter must be an English letter.
+	 * @return the frequency of a given letter in this LetterInventory.
+	 */
+	public int get(char letter) {
+		//converts char to lower case
+		char newLetter = ("" + letter).toLowerCase().charAt(0);
+		if (!('a' <= newLetter && newLetter <= 'z')) {
+			throw new IllegalArgumentException("Violation of precondtion, get."
+					+ " Parameter must be a letter of the English alphabet");
+		}
+		return frequencies[newLetter - CHAR_TO_INDEX];
+	}
+	
+	/**
+	 * Finds the number of letters in this LetterInventory.
+	 * @return the number of letters in this LetterInventory.
+	 */
+	public int size() {
+		//reset size
+	    size = 0;
+	    //find new size
+	    for (int i = 0; i < frequencies.length; i++) {
+	    	size += frequencies[i];
+	    }
+	    return size;
+	}
+	
+	/**
+	 * Finds if this LetterInventory is empty.
+	 * @return true if this LetterInventory is empty, false otherwise.
+	 */
+	public boolean isEmpty() {
+		//finds if empty
+		return size == 0;
+	}
+	
+	/**
+	 * Creates a String form of every letter in this LetterInventory in alphabetical order.
+	 * @return a String representation of this LetterInventory.
+	 */
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		//adds chars to string builder
+		for (int i = 0; i < frequencies.length; i++) {
+			for (int j = 0; j < frequencies[i]; j++) {
+				sb.append((char) (CHAR_TO_INDEX + i));
+			}
+		}
+		return sb.toString();
+	}
+	
+	/**
+	 * Creates a new LetterInventory containing both letters from this LetterInventory and another
+	 * LetterInventory.
+	 * @param otherInventory != null.
+	 * @return a new LetterInventory containing the frequencies of this LetterInventory and the
+	 * frequencies of the letters of the given LetterInventory added together.
+	 */
+	public LetterInventory add(LetterInventory otherInventory) {
+		if (otherInventory == null) {
+			throw new IllegalArgumentException("Violation of precondtion: add."
+					+ " Parameter must not be null");
+		}
+		LetterInventory newInventory = new LetterInventory("");
+		//adds frequencies from this inventory and the given inventory to new inventory
+		for (int i = 0; i < ALPHABET_SIZE; i++) {
+			newInventory.frequencies[i] = frequencies[i] + otherInventory.frequencies[i];
+			//increments size of new inventory
+			newInventory.size += newInventory.frequencies[i];
+		}
+		return newInventory;
+	}
+	
+	/**
+	 * Creates a new LetterInventory containing the letters from another LetterInventory
+	 * subtracted from this LetterInventory.
+	 * @param otherInventory != null.
+	 * @return a new LetterInventory containing the letter frequencies of the given
+	 * LetterInventory from this LetterInventory's letter frequencies.
+	 */
+	public LetterInventory subtract(LetterInventory otherInventory) {
+		if (otherInventory == null) {
+			throw new IllegalArgumentException("Violation of precondtion: add."
+					+ " Parameter must not be null");
+		}
+		LetterInventory newInventory = new LetterInventory("");
+		//subtracts frequencies from the given inventory from this inventory
+		for (int i = 0; i < ALPHABET_SIZE; i++) {
+			newInventory.frequencies[i] = frequencies[i] - otherInventory.frequencies[i];
+			//ensures the frequency is not less than 0
+			if (newInventory.frequencies[i] < 0) {
+				return null;
+			}
+			//increments size of new inventory
+			newInventory.size += newInventory.frequencies[i];
+		}
+		return newInventory;
+	}
+	
+	/**
+	 * Finds if two LetterInventory's have the same letter frequencies.
+	 * Return true if both LetterInventory's have the same letter frequencies
+	 * and false otherwise.
+	 */
+	public boolean equals(Object other) {
+		//finds if given Object is null or if it is a LetterInventory
+		if (other == null || !(other instanceof LetterInventory)) {
+			return false;
+		}
+		//gets array of other frequencies
+		int[] otherFrequencies = ((LetterInventory) other).frequencies;
+		//compares frequencies and finds if any are different
+		for (int i = 0; i < ALPHABET_SIZE; i++) {
+			if (frequencies[i] != otherFrequencies[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
